120b24e16b9de4d3ce6dda39d373221b5d07185f
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-22 08:39:25 +0000

    chore: Create json object with team member details

---
+++ components/TeamMembers/teamMembers.js
@@ -0,0 +1,50 @@
+const teamMembers = {
+  "martin": {
+    "name": "Martin Twum Mensah",
+    "role": "User Interface Developer",
+    "description": "A product manager/web developer with experience in building web applications using Python, JavaScript(Node.js, Express) and HTML/CSS; with an in-depth experience of managing cross-functional teams over 4 years on various projects.",
+    "profileImage": './img/team/martin.JPG',
+    "github": "https://github.com/twumm",
+    "twitter": "https://twitter.com/twumm"
+  },
+  "elise": {
+    "name": "Elise Spain",
+    "role": "User Interface Developer",
+    "description": "Here",
+    "profileImage": './img/team/elise.JPG',
+    "github": "https://github.com/espain16",
+    "twitter": ""
+  },
+  "cesar": {
+    "name": "Cesar Aponte",
+    "role": "User Interface Developer",
+    "description": "Here",
+    "profileImage": './img/team/cesar.JPG',
+    "github": "https://github.com/espain16",
+    "twitter": ""
+  },
+  "alfonso": {
+    "name": "Alfonso Garcia",
+    "role": "Frontend Architect",
+    "description": "Here",
+    "profileImage": './img/team/alfonso.JPG',
+    "github": "https://github.com/alfonsog714",
+    "twitter": ""
+  },
+  "duc": {
+    "name": "Duc Vu",
+    "role": "Backend Engineer",
+    "description": "Here",
+    "profileImage": './img/team/duc.JPG',
+    "github": "https://github.com/vud531",
+    "twitter": ""
+  },
+  "brandon": {
+    "name": "Brandon Desselle",
+    "role": "Backend Engineer",
+    "description": "Here",
+    "profileImage": './img/team/brandon.JPG',
+    "github": "",
+    "twitter": ""
+  }
+}
